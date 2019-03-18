eedcfcfa884622588a76bfebb689bfa4e6e0ae56
Clark <cpennie@math.utexas.edu>
2019-03-18 17:26:23 +0000

    Tidy up before merge to master
    Deleted a couple of files and made the moment_differ print success messages when moment values compared pass a test.

--- tests/moment_differ.sh
+++ tests/moment_differ.sh
@@ -28,8 +28,10 @@ m_0=$(awk 'NR == 6 {print $1}' $moment_filename_expected)
 m=$(awk 'NR == 6 {print $1}' $moment_filename_test)
 
 if [ $(echo "$m - $m_0 > $m_thresh" | bc) == 1 ] || [ $(echo "$m_0 - $m > $m_thresh" | bc) == 1 ]; then
-	echo "# Error in mass value!" >&3
+	echo "#   > Error in mass value!" >&3
 	exit 1
+else
+        echo "#   > Yes!" >&3
 fi
 
 # Read first component of velocity values after time-step 10
@@ -40,8 +42,10 @@ u1_in=$(awk 'NR == 6 {print $2}' $moment_filename_test)
 u1=$(echo "$u1_in" | sed -E 's/([+-]?[0-9.]+)[eE]\+?(-?)([0-9]+)/(\1*10^\2\3)/g')
 
 if [ $(echo "$u1 - $u1_0 > $u1_thresh" | bc -l) == 1 ] || [ $(echo "$u1_0 - $u1 > $u1_thresh" | bc -l) == 1 ]; then
-	echo "# Error in first component of momentum!" >&3
+	echo "#   > Error in first component of momentum!" >&3
 	exit 1
+else
+        echo "#   > Yes!" >&3
 fi
 
 # Read second component of velocity values after time-step 10
@@ -52,8 +56,10 @@ u2_in=$(awk 'NR == 6 {print $3}' $moment_filename_test)
 u2=$(echo "$u2_in" | sed -E 's/([+-]?[0-9.]+)[eE]\+?(-?)([0-9]+)/(\1*10^\2\3)/g')
 
 if [ $(echo "$u2 - $u2_0 > $u2_thresh" | bc -l) == 1 ] || [ $(echo "$u2_0 - $u2 > $u2_thresh" | bc -l) == 1 ]; then
-	echo "# Error in second component of momentum!" >&3
+	echo "#   > Error in second component of momentum!" >&3
 	exit 1
+else
+        echo "#   > Yes!" >&3
 fi
 
 # Read third component of velocity values after time-step 10
@@ -64,8 +70,10 @@ u3_in=$(awk 'NR == 6 {print $4}' $moment_filename_test)
 u3=$(echo "$u3_in" | sed -E 's/([+-]?[0-9.]+)[eE]\+?(-?)([0-9]+)/(\1*10^\2\3)/g')
 
 if [ $(echo "$u3 - $u3_0 > $u3_thresh" | bc -l) == 1 ] || [ $(echo "$u3_0 - $u3 > $u3_thresh" | bc -l) == 1 ]; then
-	echo "# Error in third component of momentum!" >&3
+	echo "#   > Error in third component of momentum!" >&3
 	exit 1
+else
+        echo "#   > Yes!" >&3
 fi
 
 # Read total energy values after time-step 10
@@ -73,8 +81,10 @@ echo -e "# - Comparing total energy values... \n" >&3
 T_0=$(awk 'NR == 6 {print $9}' $moment_filename_expected)
 T=$(awk 'NR == 6 {print $9}' $moment_filename_test)
 if [ $(echo "$T - $T_0 > $T_thresh" | bc) == 1 ] || [ $(echo "$T_0 - $T > $u2_thresh" | bc) == 1 ]; then
-	echo "# Error in total energy!" >&3
+	echo "#   > Error in total energy!" >&3
 	exit 1
+else
+        echo "#   > Yes!" >&3
 fi
 
 exit 0
--- tests/success.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo "wahoo!"
-
-exit 0
