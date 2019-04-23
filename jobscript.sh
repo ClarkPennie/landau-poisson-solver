4a6d73dfc980fdb0079e1983ec3accbe8b5c8ff5
Clark Pennie <cpennie@math.utexas.edu>
2019-04-23 11:56:32 -0500

    Added new input files to .gitignore
    As many input files can be added to the directory as needed now without git thinking they should be tracked.  Same with the jobscripts.

--- jobscript.sh
+++ jobscript.sh
@@ -4,14 +4,14 @@
 #SBATCH -A Boltzmann-spectral      # Charges run to <project_account_name>
 #SBATCH -o output_files/LPsolver.%j.out    # Name of the output and error file (%j expands to jobID) // only symbol % can be recoginized, others say & not
 #SBATCH -n 24     # Total number of mpi tasks requested
-#SBATCH -N 12     # The number of nodes. n/N tasks are launched on each node. used in conjunction with the -n option (above). Use this option to specify launching less than 16 tasks per node. 
-#SBATCH -p skx-normal     # Queue (partition) name  -- normal, largemem, development, gpu, etc.
-#SBATCH -t 07:00:00     # Run time (hh:mm:ss) 
+#SBATCH -N 24     # The number of nodes. n/N tasks are launched on each node. used in conjunction with the -n option (above). Use this option to specify launching less than 16 tasks per node. 
+#SBATCH -p normal     # Queue (partition) name  -- normal, largemem, development, gpu, etc.
+#SBATCH -t 04:00:00     # Run time (hh:mm:ss) 
 #SBATCH --mail-user=clark.pennie@utexas.edu     #Specify the email address to use for notifications.
 #SBATCH --mail-type=ALL       #Specify when user notifications are to be sent.
 
 set -x
-export OMP_NUM_THREADS=24
+export OMP_NUM_THREADS=68
 
 #ibrun tacc_affinity ./bin/LPsolver_nu005_QLinear_BCTempsEqual_eps10^-1.out
 ibrun tacc_affinity ./source/solver
